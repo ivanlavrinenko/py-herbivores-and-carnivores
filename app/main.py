f5222216a0848258d1806cc639b8740e6d506c49
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2025-11-27 02:36:44 +0100

    return type annotation added for bite method

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ class Herbivore(Animal):
 
 
 class Carnivore(Animal):
-    def bite(self, herbivore: Herbivore):
+    def bite(self, herbivore: Herbivore) -> None:
         if not herbivore.hidden and isinstance(herbivore, Herbivore):
             herbivore.health -= 50
             if herbivore.health <= 0:
